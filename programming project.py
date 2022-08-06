e823be21f470fbf30a7eff8d1e75818d66da1b0c
ayanalikhan3705 <108728513+ayanalikhan3705@users.noreply.github.com>
2022-08-06 20:32:44 +0530

    Add files via upload

---
+++ programming project.py
@@ -0,0 +1,25 @@
+#write a program to print hello world
+
+print('Hello world')
+
+#write a program to do arithmetical operations addition and division.?
+a , b = 20 ,10
+print(a+b , a/b)
+
+#write a program to calculate area of triangle
+base = int(input('enter a base of triangle'))
+height = int(input('enter a height of triangle'))
+area_of_triangle = 1/2 *base*height
+
+#write a program to swap two variables?
+a = int(input('enter a number'))
+b = int(input('enter a number'))
+print(a,b ,'before the swap value')
+a , b =  b , a
+print(a , b, 'after the value swap')
+
+#write a program to generate random number?
+import random
+a = random.random()
+print(a)
+
